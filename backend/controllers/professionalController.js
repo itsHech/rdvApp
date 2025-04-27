6fad776279eb96f1d457b5084b4007c60a201d22
hechmi <hechmi@80-FA-5B-70-D0-ED.home>
2025-04-27 17:33:46 +0100

    backend : get professionalById

--- backend/controllers/professionalController.js
+++ backend/controllers/professionalController.js
@@ -1,14 +1,26 @@
 const Professional = require("../models/Professional");
-const User = require('../models/User');
+const User = require("../models/User");
 
 // Get all professionals
 exports.getProfessionals = async (req, res) => {
   try {
     const professionals = await User.find({
-        role : 'professional'
+      role: "professional",
     });
     res.json(professionals);
   } catch (err) {
     res.status(500).json({ error: err });
+  }
+};
+
+exports.getProfessionalById = async (req, res) => {
+  try {
+    const professional = await User.find({
+      $and: [{ _id: req.params.id }, { role: "professional" }],
+    });
+    if (!professional) return res.status(404).json({ error: "Not found" });
+    res.json(professional);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
   }
 };
--- backend/routes/professional.js
+++ backend/routes/professional.js
@@ -4,5 +4,6 @@ const professionalController = require('../controllers/professionalController');
 const authenticateToken = require('../middleware/authMiddleware');
 
 router.get('/', authenticateToken, professionalController.getProfessionals);
+router.get('/:id', authenticateToken, professionalController.getProfessionalById);
 
 module.exports = router;
